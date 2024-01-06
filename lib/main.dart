fcad26f52f1f0bc95b51fb2bb0e1daf7eed36ba9
tareklaaouane <tlaaouane@gmail.com>
2024-01-06 15:37:31 +0100

    currency converter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:tutoapp/currency_converter.dart';
 
 void main() {
-  runApp(Text("hello world", textDirection: TextDirection.ltr));
+  runApp(const app());
+}
+
+class app extends StatelessWidget {
+  const app({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: CurrencyConverter()
+    );
+  }
 }
