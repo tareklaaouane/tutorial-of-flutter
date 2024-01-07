9f283d2bf1510c93d0fe07678d2d658d5f842cbc
tareklaaouane <tlaaouane@gmail.com>
2024-01-07 02:50:14 +0100

    tuto #2

--- lib/currency_converter.dart
+++ lib/currency_converter.dart
@@ -2,10 +2,39 @@ import 'package:flutter/material.dart';
 
 class CurrencyConverter extends StatelessWidget {
   const CurrencyConverter({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(child: Text("hello world")),
+      backgroundColor: Colors.black,
+      body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+        Text(
+          "hello world ",
+          style: TextStyle(
+              fontSize: 25, fontWeight: FontWeight.bold, color: Colors.amber),
+        ),
+        TextField(
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+            color: Color.fromARGB(255, 255, 255, 255),
+          ),
+          decoration:InputDecoration(
+            label: Text("please enter here the amount in mad"
+            ,style: TextStyle(
+              color: Colors.white,
+            ),
+            ),
+            hintText: "the value in mad ",
+            
+            
+          ),
+
+        )
+      ])),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:tutoapp/currency_converter.dart';
 
 void main() {
-  runApp(const app());
+  runApp(const App());
 }
 
-class app extends StatelessWidget {
-  const app({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
