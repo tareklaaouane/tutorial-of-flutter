2aec69e1c81dd0d49d1c6e964fa4d7d256e3c6b3
tareklaaouane <tlaaouane@gmail.com>
2024-01-09 17:14:48 +0100

    elevated button

--- lib/currency_converter.dart
+++ lib/currency_converter.dart
@@ -18,61 +18,69 @@ class CurrencyConverter extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "hello world ",
-              style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.amber),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(10.0),
-              child: TextField(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "hello world ",
                 style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                  color: Color.fromARGB(255, 255, 255, 255),
-                ),
-                decoration: InputDecoration(
-                  /* label: Text("please enter here the amount in mad"
-              ,style: TextStyle(
-                color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.amber),
               ),
-              ), */
-                  hintText: "the value in mad ",
-                  hintStyle: TextStyle(
-                    color: Color.fromARGB(255, 0, 137, 250),
+              const Padding(
+                padding: EdgeInsets.all(10.0),
+                child: TextField(
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    color: Color.fromARGB(255, 255, 255, 255),
                   ),
-                  prefixIcon: Icon(Icons.monetization_on),
-                  prefixIconColor: Colors.amber,
-                  suffixIcon: Icon(Icons.attach_money),
-                  suffixIconColor: Colors.amber,
-                  filled: true,
-                  fillColor: Color.fromARGB(255, 80, 80, 80),
-                  focusedBorder: border,
-                  enabledBorder: border,
+                  decoration: InputDecoration(
+                    /* label: Text("please enter here the amount in mad"
+                ,style: TextStyle(
+                  color: Colors.white,
                 ),
-                keyboardType: TextInputType.numberWithOptions(
-                  decimal: true,
+                ), */
+                    hintText: "the value in mad ",
+                    hintStyle: TextStyle(
+                      color: Color.fromARGB(255, 0, 137, 250),
+                    ),
+                    prefixIcon: Icon(Icons.monetization_on),
+                    prefixIconColor: Colors.amber,
+                    suffixIcon: Icon(Icons.attach_money),
+                    suffixIconColor: Colors.amber,
+                    filled: true,
+                    fillColor: Color.fromARGB(255, 80, 80, 80),
+                    focusedBorder: border,
+                    enabledBorder: border,
+                  ),
+                  keyboardType: TextInputType.numberWithOptions(
+                    decimal: true,
+                  ),
                 ),
               ),
-            ),
-            TextButton(
-              onPressed: () {
-                debugPrint("object");
-              },
-              style: const ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll(Colors.amber),
-                  foregroundColor: MaterialStatePropertyAll(Colors.black)),
-              child: const Text(
-                "convert",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ElevatedButton(
+                onPressed: () {
+                  debugPrint("object");
+                },
+                style: const ButtonStyle(
+                    elevation: MaterialStatePropertyAll(15),
+                    shadowColor: MaterialStatePropertyAll(
+                        Color.fromARGB(142, 255, 193, 7)),
+                    backgroundColor: MaterialStatePropertyAll(Colors.amber),
+                    foregroundColor: MaterialStatePropertyAll(Colors.black),
+                    maximumSize:
+                        MaterialStatePropertyAll(Size(double.infinity, 50))),
+                child: const Text(
+                  "convert",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
